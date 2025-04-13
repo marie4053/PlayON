49ae9c1d7661dc22ccce83fe473e64c3ddc59852
Sang Yup Kim <kylekim95@gmail.com>
2025-04-13 23:01:38 +0900

    230 feat 자유게시판 api (#239)
    * feat: files
    
    * fix endpoints
    
    * feat : free community api
    
    * feat : fix use router bug

---
+++ src/constants/endpoints/free-community.ts
@@ -0,0 +1,15 @@
+export const FREECOMMUNITY_ENDPOINTS = Object.freeze({
+  postDetail: (boardId: number) => `/boards/${boardId}`,
+  postUpdate: (boardId: number) => `/boards/${boardId}`,
+  postDelete: (boardId: number) => `/boards/${boardId}`,
+  postCreate: `/boards`,
+  postImg: (boardId: number) => `/boards/${boardId}/img`,
+  postList: `/board/list`,
+  postLike: (boardId: number) => `/boards/${boardId}/like`,
+  postUnlike: (boardId: number) => `/boards/${boardId}/like`,
+
+  commentUpdate: (boardId: number, commentId: number) => `/boards/${boardId}/comments/${commentId}`,
+  commentDelete: (boardId: number, commentId: number) => `/boards/${boardId}/comments/${commentId}`,
+  commentGet: (boardId: number) => `/boards/${boardId}/comments`,
+  commentCreate: (boardId: number) => `/boards/${boardId}/comments`,
+});
