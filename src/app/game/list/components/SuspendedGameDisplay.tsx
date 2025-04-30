e605f492132fadf3712667a54d586c4017247733
SangYupKim <kylekim95@gmail.com>
2025-04-30 16:33:49 +0900

    [HOTFIX] : game list interactivity

---
+++ src/app/game/list/components/SuspendedGameDisplay.tsx
@@ -0,0 +1,102 @@
+import { game, party, partyLog } from '@/api/game';
+import { gameDetail } from '@/types/games';
+import CustomPagination from '@/components/common/CustomPagination';
+import EmptyLottie from '@/components/common/EmptyLottie';
+import PickCard from '@/components/game/PickCard';
+import ImFeelingLuckyBtn from './ImFeelingLuckyBtn';
+import axios from 'axios';
+import { GAME_ENDPOINTS } from '@/constants/endpoints/game';
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+type Step = {
+  keyword: string;
+  isMacSupported: string;
+  releaseDate: string;
+  playerType: string;
+  releaseStatus: string;
+  genres: (string | undefined)[];
+};
+type Pagination = {
+  page: number;
+  size: number;
+  sort: string[];
+};
+async function fetchGameScreenshots(
+  step: Step,
+  pagination: Pagination
+): Promise<{ totalItems: number; appIds: number[]; validData: gameDetail[] }> {
+  const response = await axios.post(API_BASE_URL + GAME_ENDPOINTS.list, { ...step }, { params: { ...pagination } });
+  const totalItems = response.data.data.totalItems;
+  const appIds: number[] = response.data.data.items.map(
+    (_: { appid: number; name: string; headerImage: string; genres: string[] }) => _.appid
+  );
+  const gameData = await Promise.all(
+    appIds.map(async (appid: number) => {
+      const detailResponse = await axios.get(API_BASE_URL + GAME_ENDPOINTS.details(appid));
+      if (detailResponse && detailResponse.status === 200) {
+        return {
+          game: detailResponse.data.data.game as game,
+          partyList: detailResponse.data.data.partyList as party[],
+          partyLogList: detailResponse.data.data.completedPartyList as partyLog[],
+        };
+      }
+      return undefined;
+    })
+  );
+  function convertToClientGame(data: game): gameDetail {
+    return {
+      about: data.aboutTheGame,
+      detail_desc: data.aboutTheGame,
+      developer: [data.developers],
+      genre: data.genres,
+      homepage_url: data.website,
+      img_src: data.headerImage,
+      movie_src: data.movies,
+      screenshot_src: data.screenshots,
+      os: {
+        linux: data.isLinuxSupported,
+        mac: data.isMacSupported,
+        windows: data.isWindowsSupported,
+      },
+      publisher: [data.publishers],
+      release_date: data.releaseDate,
+      short_desc: data.shortDescription,
+      title: data.name,
+    };
+  }
+  const validData = gameData.filter((_) => _ !== undefined).map((__) => convertToClientGame(__.game));
+  return {
+    totalItems,
+    appIds,
+    validData,
+  };
+}
+
+export default async function SuspendedGameDisplay(props: { step: Step; pagination: Pagination }) {
+  const { totalItems, appIds, validData } = await fetchGameScreenshots(props.step, props.pagination);
+
+  return (
+    <>
+      {validData && validData.length > 0 && (
+        <div className="lg:w-[1280px] grid grid-cols-4 grid-rows-3 gap-x-6 gap-y-12 mt-[100px]">
+          {validData.map((e, ind) => (
+            <PickCard data={e} key={ind} appid={appIds[ind]} />
+          ))}
+        </div>
+      )}
+      {validData && validData.length <= 0 && (
+        <div className="w-full text-center mt-[100px] mb-[100px]">
+          <EmptyLottie className="w-[400px]" text="원하는 게임이 없으신가요?">
+            <ImFeelingLuckyBtn />
+          </EmptyLottie>
+        </div>
+      )}
+      {validData && validData.length > 0 && (
+        <div className="mt-[100px] mb-[100px]">
+          <CustomPagination pageSize={12} totalItems={totalItems} />
+        </div>
+      )}
+    </>
+  );
+}
--- src/app/game/list/page.tsx
+++ src/app/game/list/page.tsx
@@ -2,23 +2,15 @@ import HeroSwiperBanner from '@/components/common/HeroSwiperBanner';
 import CustomGameSearch from './components/CustomGameSearch';
 import CustomMacSwitch from './components/CustomMacSwitch';
 import { CustomDateTimePicker } from './components/CustomDateTimePicker';
-import axios from 'axios';
-import { GAME_ENDPOINTS } from '@/constants/endpoints/game';
 import CustomReleaseStatus from './components/CustomReleaseStatus';
 import CustomPlayerType from './components/CustomPlayerType';
 import CustomGenres from './components/CustomGenres';
-import { game, party, partyLog } from '@/api/game';
-import { gameDetail } from '@/types/games';
-import CustomPagination from '@/components/common/CustomPagination';
-import EmptyLottie from '@/components/common/EmptyLottie';
-import PickCard from '@/components/game/PickCard';
-import ImFeelingLuckyBtn from './components/ImFeelingLuckyBtn';
 import typeConverter from '@/utils/typeConverter';
 import CustomResetComponent from './components/CustomResetComponent';
 import { Suspense } from 'react';
 import { Skeleton } from '@/components/ui/skeleton';
+import SuspendedGameDisplay from './components/SuspendedGameDisplay';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 const imageList = [
   { title: 'Bioshock: Infinite', img_src: '/img/hero/bg_gameList_1.webp' },
   { title: 'Cuphead', img_src: '/img/hero/bg_gameList_2.webp' },
@@ -29,67 +21,6 @@ const imageList = [
 const genres = ['액션', '인디', '어드벤처', '시뮬레이션', 'RPG', '전략', '캐주얼'] as const;
 const playerTypes = ['멀티플레이', '싱글플레이'] as const;
 const releaseStatuses = ['발매', '출시예정'] as const;
-
-type Step = {
-  keyword: string;
-  isMacSupported: string;
-  releaseDate: string;
-  playerType: string;
-  releaseStatus: string;
-  genres: (string | undefined)[];
-};
-type Pagination = {
-  page: number;
-  size: number;
-  sort: string[];
-};
-async function fetchGameScreenshots(
-  step: Step,
-  pagination: Pagination
-): Promise<[totalItems: number, appIds: number[], validData: gameDetail[]]> {
-
-  const response = await axios.post(API_BASE_URL + GAME_ENDPOINTS.list, { ...step }, { params: { ...pagination } });
-  const totalItems = response.data.data.totalItems;
-  const appIds: number[] = response.data.data.items.map(
-    (_: { appid: number; name: string; headerImage: string; genres: string[] }) => _.appid
-  );
-  const gameData = await Promise.all(
-    appIds.map(async (appid: number) => {
-      const detailResponse = await axios.get(API_BASE_URL + GAME_ENDPOINTS.details(appid));
-      if (detailResponse && detailResponse.status === 200) {
-        return {
-          game: detailResponse.data.data.game as game,
-          partyList: detailResponse.data.data.partyList as party[],
-          partyLogList: detailResponse.data.data.completedPartyList as partyLog[],
-        };
-      }
-      return undefined;
-    })
-  );
-  function convertToClientGame(data: game): gameDetail {
-    return {
-      about: data.aboutTheGame,
-      detail_desc: data.aboutTheGame,
-      developer: [data.developers],
-      genre: data.genres,
-      homepage_url: data.website,
-      img_src: data.headerImage,
-      movie_src: data.movies,
-      screenshot_src: data.screenshots,
-      os: {
-        linux: data.isLinuxSupported,
-        mac: data.isMacSupported,
-        windows: data.isWindowsSupported,
-      },
-      publisher: [data.publishers],
-      release_date: data.releaseDate,
-      short_desc: data.shortDescription,
-      title: data.name,
-    };
-  }
-  const validData = gameData.filter((_) => _ !== undefined).map((__) => convertToClientGame(__.game));
-  return [totalItems, appIds, validData];
-}
 
 export default async function GameList({
   searchParams,
@@ -121,8 +52,6 @@ export default async function GameList({
     size: 12,
     sort: [],
   };
-  const [totalItems, appIds, validData] = await fetchGameScreenshots(step, pagination);
-
   const PickCardSkeletons = () => {
     return (
       <div className="lg:w-[1280px] grid grid-cols-4 grid-rows-3 gap-x-6 gap-y-12 mt-[100px] mb-[100px]">
@@ -132,7 +61,6 @@ export default async function GameList({
       </div>
     );
   };
-
   return (
     <div className="flex flex-col items-center">
       <section className="w-full h-[520px]">
@@ -194,25 +122,7 @@ export default async function GameList({
         </div>
       </div>
       <Suspense fallback={<PickCardSkeletons />}>
-        {validData && validData.length > 0 && (
-          <div className="lg:w-[1280px] grid grid-cols-4 grid-rows-3 gap-x-6 gap-y-12 mt-[100px]">
-            {validData.map((e, ind) => (
-              <PickCard data={e} key={ind} appid={appIds[ind]} />
-            ))}
-          </div>
-        )}
-        {validData && validData.length <= 0 && (
-          <div className="w-full text-center mt-[100px] mb-[100px]">
-            <EmptyLottie className="w-[400px]" text="원하는 게임이 없으신가요?">
-              <ImFeelingLuckyBtn />
-            </EmptyLottie>
-          </div>
-        )}
-        {validData && validData.length > 0 && (
-          <div className="mt-[100px] mb-[100px]">
-            <CustomPagination pageSize={12} totalItems={totalItems} />
-          </div>
-        )}
+        <SuspendedGameDisplay pagination={pagination} step={step} />
       </Suspense>
     </div>
   );
