0da0063fffeb329a3772177359a39b5990f9f6de
SangYupKim <kylekim95@gmail.com>
2025-04-30 16:54:21 +0900

    [HOTFIX] game list interactivity 2nd attempt

--- src/app/game/list/components/SuspendedGameDisplay.tsx
+++ src/app/game/list/components/SuspendedGameDisplay.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { game, party, partyLog } from '@/api/game';
 import { gameDetail } from '@/types/games';
 import CustomPagination from '@/components/common/CustomPagination';
@@ -6,6 +8,7 @@ import PickCard from '@/components/game/PickCard';
 import ImFeelingLuckyBtn from './ImFeelingLuckyBtn';
 import axios from 'axios';
 import { GAME_ENDPOINTS } from '@/constants/endpoints/game';
+import { useEffect, useState } from 'react';
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
@@ -22,59 +25,61 @@ type Pagination = {
   size: number;
   sort: string[];
 };
-async function fetchGameScreenshots(
-  step: Step,
-  pagination: Pagination
-): Promise<{ totalItems: number; appIds: number[]; validData: gameDetail[] }> {
-  const response = await axios.post(API_BASE_URL + GAME_ENDPOINTS.list, { ...step }, { params: { ...pagination } });
-  const totalItems = response.data.data.totalItems;
-  const appIds: number[] = response.data.data.items.map(
-    (_: { appid: number; name: string; headerImage: string; genres: string[] }) => _.appid
-  );
-  const gameData = await Promise.all(
-    appIds.map(async (appid: number) => {
-      const detailResponse = await axios.get(API_BASE_URL + GAME_ENDPOINTS.details(appid));
-      if (detailResponse && detailResponse.status === 200) {
+
+export default function SuspendedGameDisplay(props: { step: Step; pagination: Pagination }) {
+  // const { totalItems, appIds, validData } = fetchGameScreenshots(props.step, props.pagination);
+  const [totalItems, setTotalItems] = useState(48);
+  const [appIds, setAppIds] = useState<number[]>([]);
+  const [validData, setValidData] = useState<gameDetail[]>();
+
+  useEffect(() => {
+    async function fetchGameScreenshots(step: Step, pagination: Pagination) {
+      const response = await axios.post(API_BASE_URL + GAME_ENDPOINTS.list, { ...step }, { params: { ...pagination } });
+      const totalItems = response.data.data.totalItems;
+      const appIds: number[] = response.data.data.items.map(
+        (_: { appid: number; name: string; headerImage: string; genres: string[] }) => _.appid
+      );
+      const gameData = await Promise.all(
+        appIds.map(async (appid: number) => {
+          const detailResponse = await axios.get(API_BASE_URL + GAME_ENDPOINTS.details(appid));
+          if (detailResponse && detailResponse.status === 200) {
+            return {
+              game: detailResponse.data.data.game as game,
+              partyList: detailResponse.data.data.partyList as party[],
+              partyLogList: detailResponse.data.data.completedPartyList as partyLog[],
+            };
+          }
+          return undefined;
+        })
+      );
+      function convertToClientGame(data: game): gameDetail {
         return {
-          game: detailResponse.data.data.game as game,
-          partyList: detailResponse.data.data.partyList as party[],
-          partyLogList: detailResponse.data.data.completedPartyList as partyLog[],
+          about: data.aboutTheGame,
+          detail_desc: data.aboutTheGame,
+          developer: [data.developers],
+          genre: data.genres,
+          homepage_url: data.website,
+          img_src: data.headerImage,
+          movie_src: data.movies,
+          screenshot_src: data.screenshots,
+          os: {
+            linux: data.isLinuxSupported,
+            mac: data.isMacSupported,
+            windows: data.isWindowsSupported,
+          },
+          publisher: [data.publishers],
+          release_date: data.releaseDate,
+          short_desc: data.shortDescription,
+          title: data.name,
         };
       }
-      return undefined;
-    })
-  );
-  function convertToClientGame(data: game): gameDetail {
-    return {
-      about: data.aboutTheGame,
-      detail_desc: data.aboutTheGame,
-      developer: [data.developers],
-      genre: data.genres,
-      homepage_url: data.website,
-      img_src: data.headerImage,
-      movie_src: data.movies,
-      screenshot_src: data.screenshots,
-      os: {
-        linux: data.isLinuxSupported,
-        mac: data.isMacSupported,
-        windows: data.isWindowsSupported,
-      },
-      publisher: [data.publishers],
-      release_date: data.releaseDate,
-      short_desc: data.shortDescription,
-      title: data.name,
-    };
-  }
-  const validData = gameData.filter((_) => _ !== undefined).map((__) => convertToClientGame(__.game));
-  return {
-    totalItems,
-    appIds,
-    validData,
-  };
-}
-
-export default async function SuspendedGameDisplay(props: { step: Step; pagination: Pagination }) {
-  const { totalItems, appIds, validData } = await fetchGameScreenshots(props.step, props.pagination);
+      const validData = gameData.filter((_) => _ !== undefined).map((__) => convertToClientGame(__.game));
+      setTotalItems(totalItems);
+      setAppIds(appIds);
+      setValidData(validData);
+    }
+    fetchGameScreenshots(props.step, props.pagination);
+  }, [props.step, props.pagination]);
 
   return (
     <>
